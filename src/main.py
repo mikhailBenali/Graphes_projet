5d72e27be78336ac576f1cbc2990f7492431d698
mikhailBenali <94171094+mikhailBenali@users.noreply.github.com>
2024-03-29 18:19:23 +0100

    création et affichage de la matrice

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
+sommets, arretes, predecesseurs = [], [], []
+
+# Lecture du fichier et stockage des données
+
 with open('graphes/test.txt', 'r') as f:
-    data = f.read()
-    print(data)
+    for ligne in f:
+        ligne = ligne.replace("\n", "")
+        ligne = ligne.split(" ")
+        sommets.append(ligne[0])
+        arretes.append(ligne[1])
+        predecesseurs.append(ligne[2:])
+
+# Création de la matrice des valeurs
+
+matrice_adjacence = [[-1 for i in range(len(sommets)+2)] for j in range(len(sommets)+2)] # On initialise la matrice à -1 en ajoutant 2 sommets pour alpha et omega
+
+
+def afficher_matrice(matrice):
+        for ligne in matrice:
+            print(ligne)
+
